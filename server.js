fdc0cbe83852ab043f69469635db193573fed79c
David Meir-Levy <davidmeirlevy@gmail.com>
2020-04-29 20:18:00 +0300

    add cache for production admin panel

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3001
 const base = process.env.BASE_URL || 'gp-admin'
 
-app.use(base, express.static(path.join(__dirname, 'dist')))
+app.use(base, express.static(path.join(__dirname, 'dist'), { maxAge: '30d' }))
 app.use('*', express.static(path.join(__dirname, 'dist/index.html')))
 
 app.listen(port, () => console.log('Admin front-server is up on port ' + port + base))
