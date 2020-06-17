6926ffee8894576e44d5f3bbf02b728bea53c685
David Meir-Levy <davidmeirlevy@gmail.com>
2020-06-17 11:27:41 +0300

    admin panel as npm package

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3001
-const base = process.env.BASE_URL || 'gp-admin'
+const base = process.env.BASE_URL || '/gp-admin'
 
 app.use(base, express.static(path.join(__dirname, 'dist'), { maxAge: '30d' }))
 app.use('*', express.static(path.join(__dirname, 'dist/index.html')))
 
-app.listen(port, () => console.log('Admin front-server is up on port ' + port + base))
+app.listen(port, () => console.log('Admin front-server is up on port ' + port, base))
