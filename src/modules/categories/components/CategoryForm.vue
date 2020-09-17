2983565ab2a6dea52e596993c25b97b9d4b6b5ac
UriShX <usdogi@gmail.com>
2020-09-17 18:36:43 +0300

    Category content edit implemented

--- src/modules/categories/components/CategoryForm.vue
+++ src/modules/categories/components/CategoryForm.vue
@@ -1,35 +1,52 @@
 <template>
-	<el-form class="category-form" @submit.native.prevent="submit">
-		<div>
-			<el-checkbox label="Public?" :size="'small'" :checked="isPublic" @change="editedCategory.isPublic = $event"/>
-		</div>
-		<FormInput title="Name" :value="name" @input="editedCategory.name = $event"/>
-		<FormInput title="Path" label="leave empty to auto-generate" :value="path"
-		           @input="editedCategory.path = $event"/>
-		<el-button native-type="submit" :loading="submitting">SAVE</el-button>
-	</el-form>
+  <el-form class="category-form" @submit.native.prevent="submit">
+    <div>
+      <el-checkbox
+        label="Public?"
+        :size="'small'"
+        :checked="isPublic"
+        @change="editedCategory.isPublic = $event"
+      />
+    </div>
+    <FormInput
+      title="Name"
+      :value="name"
+      @input="editedCategory.name = $event"
+    />
+    <FormInput
+      title="Path"
+      label="leave empty to auto-generate"
+      :value="path"
+      @input="editedCategory.path = $event"
+    />
+    <el-form-item label="Content" class="form-item-flex">
+      <div>
+        <gp-editor :value="content" @input="editedCategory.content = $event" />
+      </div>
+    </el-form-item>
+    <el-button native-type="submit" :loading="submitting">SAVE</el-button>
+  </el-form>
 </template>
 <script>
-  import FormInput from '../../core/components/forms/FormInput'
-  import { clearNulls } from '../../core/utils/clear-nulls'
-  import { useCategoryForm } from '../compositions/categories'
+import FormInput from "../../core/components/forms/FormInput"
+import { clearNulls } from "../../core/utils/clear-nulls"
+import { useCategoryForm } from "../compositions/categories"
 
-  export default {
-    name: 'CategoryForm',
-    components: { FormInput },
-    props: {
-      category: Object,
-      submitting: Boolean
-    },
-    setup (props, { emit }) {
-      const data = useCategoryForm(props)
+export default {
+  name: "CategoryForm",
+  components: { FormInput },
+  props: {
+    category: Object,
+    submitting: Boolean
+  },
+  setup(props, { emit }) {
+    const data = useCategoryForm(props)
 
-      return {
-        ...data,
-        submit: () => emit('submit', clearNulls(data.editedCategory))
-      }
+    return {
+      ...data,
+      submit: () => emit("submit", clearNulls(data.editedCategory))
     }
   }
+}
 </script>
-<style scoped>
-</style>
+<style scoped></style>
--- src/modules/categories/compositions/categories.js
+++ src/modules/categories/compositions/categories.js
@@ -1,27 +1,37 @@
-import { computed, reactive, onMounted } from '@vue/composition-api'
-import { addCategory, categoriesStore, fetchCategories, removeCategory } from '../store/categories'
-import { useSubmitting } from '../../core/compositions/submitting'
-import categoriesService from '../../../services/categories-service'
-import { useDispatcher } from '../../core/compositions/dispatcher'
+import { computed, reactive, onMounted } from "@vue/composition-api"
+import {
+  addCategory,
+  categoriesStore,
+  fetchCategories,
+  removeCategory
+} from "../store/categories"
+import { useSubmitting } from "../../core/compositions/submitting"
+import categoriesService from "../../../services/categories-service"
+import { useDispatcher } from "../../core/compositions/dispatcher"
 
-function useCategories () {
+function useCategories() {
   return computed(() => categoriesStore.categories)
 }
 
-export function createCategory (category) {
+export function createCategory(category) {
   return categoriesService.create(category).then((category) => {
     addCategory(category)
     return category
   })
 }
 
-export function useEditCategory (categoryPath) {
-  const { result: category } = useDispatcher(() => categoriesService.getOne(categoryPath))
+export function useEditCategory(categoryPath) {
+  const { result: category } = useDispatcher(() =>
+    categoriesService.getOne(categoryPath)
+  )
 
-  const { submit, submitting } = useSubmitting((payload) => categoriesService.update(categoryPath, payload), {
-    success: 'Category updated successfully',
-    error: 'Failed to update category'
-  })
+  const { submit, submitting } = useSubmitting(
+    (payload) => categoriesService.update(categoryPath, payload),
+    {
+      success: "Category updated successfully",
+      error: "Failed to update category"
+    }
+  )
 
   return {
     category,
@@ -30,14 +40,16 @@ export function useEditCategory (categoryPath) {
   }
 }
 
-export function useCategoriesList () {
+export function useCategoriesList() {
   fetchCategories()
   const { submit } = useSubmitting(
-    ({ path }) => categoriesService.remove(path).then(() => removeCategory(path)),
+    ({ path }) =>
+      categoriesService.remove(path).then(() => removeCategory(path)),
     {
-      success: 'Category removed successfully',
-      error: 'Failed to remove category'
-    })
+      success: "Category removed successfully",
+      error: "Failed to remove category"
+    }
+  )
 
   return {
     categories: useCategories(),
@@ -45,11 +57,12 @@ export function useCategoriesList () {
   }
 }
 
-export function useCategoryForm (props) {
+export function useCategoryForm(props) {
   const editedCategory = reactive({
     name: null,
     path: null,
-    isPublic: null,
+    content: null,
+    isPublic: null
   })
   onMounted(() => {
     if (!props.category._id) {
@@ -57,28 +70,40 @@ export function useCategoryForm (props) {
     }
   })
   const name = computed(() => {
-    return editedCategory.name === null ? props.category.name : editedCategory.name
+    return editedCategory.name === null
+      ? props.category.name
+      : editedCategory.name
   })
   const path = computed(() => {
-    return editedCategory.path === null ? props.category.path : editedCategory.path
+    return editedCategory.path === null
+      ? props.category.path
+      : editedCategory.path
+  })
+  const content = computed(() => {
+    return editedCategory.content === null
+      ? props.category.content
+      : editedCategory.content
   })
   const isPublic = computed(() => {
-    return editedCategory.isPublic === null ? props.category.isPublic : editedCategory.isPublic
+    return editedCategory.isPublic === null
+      ? props.category.isPublic
+      : editedCategory.isPublic
   })
 
   return {
     editedCategory,
     name,
     path,
+    content,
     isPublic
   }
 }
 
-export function useCategorySelector () {
+export function useCategorySelector() {
   fetchCategories()
 
   return {
     mounted: computed(() => categoriesStore.loaded),
-    categories: useCategories(),
+    categories: useCategories()
   }
 }
