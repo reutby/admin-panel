294e5bd7b8aba63e759325a5d210368fd6bc8e65
David Meir-Levy <davidmeirlevy@gmail.com>
2020-09-18 17:12:06 +0300

    minot fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 // prettier-ignore
 module.exports = {
-	root: true,
-	env: {
-		node: true
-	},
-	'extends': [
-		'plugin:vue/essential',
-		'@vue/standard'
-	],
-	plugins: [
-		'prettier'
-	],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-tabs': 'off',
-		'no-trailing-spaces': 'off',
-		'object-curly-spacing': 'error',
-		'no-return-assign': 'off',
-		'indent': 'off',
-		'comma-dangle': 'off',
-		'no-mixed-spaces-and-tabs': 'off',
-		'vue/script-indent': ['warn', 2, { 'baseIndent': 1 }],
-		'space-before-function-paren': ['error', {
-			'anonymous': 'never',
-			'named': 'never',
-			'asyncArrow': 'always'
-		}],
-	},
-	parserOptions: {
-		parser: 'babel-eslint',
-		ecmaFeatures: {
-			legacyDecorators: true
-		}
-	}
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-tabs': 'off',
+    'no-trailing-spaces': 'off',
+    'object-curly-spacing': 'error',
+    'no-return-assign': 'off',
+    'indent': 'off',
+    'comma-dangle': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'vue/script-indent': ['warn', 2, { 'baseIndent': 1 }],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never',
+      'named': 'never',
+      'asyncArrow': 'always'
+    }],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+  }
 }
