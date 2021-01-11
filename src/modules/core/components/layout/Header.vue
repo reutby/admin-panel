9725440f24598964521d8c874dae8e63b5e38776
Oryam <48219965+or-yam@users.noreply.github.com>
2021-01-11 12:05:39 +0200

    "welcome user" translated as a sentence

--- src/modules/core/components/layout/Header.vue
+++ src/modules/core/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<header :dir="$t('appDirection')">
 		<el-button type="default" icon="el-icon-menu" class="btn" circle @click="open"/>
-		<span class="user-welcome" v-if="user">{{$t('Hello')}} <strong>{{user.name}}</strong></span>
+		<span class="user-welcome" v-if="user" v-html="$t('welcome user message',{userName:user.name})"></span>
 		<div class="actions">
 			<a @click="logout">{{$t('Logout')}}</a>
 		</div>
