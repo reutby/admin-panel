00fb957b6d04f5d12f20091f87ee999ca3debb40
Oryam <48219965+or-yam@users.noreply.github.com>
2021-01-22 17:39:30 +0200

    added translation to lable

--- src/modules/core/components/forms/FormInput.vue
+++ src/modules/core/components/forms/FormInput.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<el-form-item :label="$t(title)">
-			<small v-if="label"> ({{label}})</small>
+			<small v-if="label"> ({{$t(label)}})</small>
 			<el-input-number v-if="type === 'number'" v-on="$listeners" :value="value"/>
 			<AssetUploader v-else-if="type === 'upload'" v-on="$listeners" :value="value" class="asset-upload"/>
 			<el-input v-else v-on="$listeners" :value="value" :native-type="type" :type="type"/>
